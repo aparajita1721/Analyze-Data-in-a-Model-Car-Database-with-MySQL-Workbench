11aa8126e852532a73f34216f7e6d5bce6ac543e
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 22:44:08 +0530

    Delete priceReduction.sql

--- priceReduction.sql
+++
@@ -1,6 +0,0 @@
--- price reduction by 5 percent for the products that were sold the least
-SELECT products.productName, products.buyPrice, products.MSRP, orderdetails.priceEach, 
-(orderdetails.priceEach*0.95) AS Price_Reduction 
-FROM products JOIN orderdetails ON orderdetails.productCode=products.productCode 
-GROUP BY products.productName 
-ORDER BY orderdetails.quantityOrdered ASC LIMIT 10;
