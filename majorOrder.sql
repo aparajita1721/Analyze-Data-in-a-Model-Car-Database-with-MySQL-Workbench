515f4645955457369d2230a772c8a3e211cbcde7
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 18:56:50 +0530

    Delete majorOrder.sql

--- majorOrder.sql
+++
@@ -1,5 +0,0 @@
-/* a major client seemed to have cancelled their order as they recieve a better offer, 
-depending on the offer given by our competitor, we could bring down the pricing of each of the products by 5%*/
-SELECT orders.status, orders.customerNumber, orderdetails.quantityOrdered, orderdetails.priceEach, orders.comments 
-FROM orders JOIN orderdetails ON orders.orderNumber= orderdetails.orderNumber 
-WHERE orders.status="Cancelled" AND orders.customerNumber=141;
