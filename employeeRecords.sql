650feca876e2bd33980457729142d63f51f4ddf9
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 18:56:04 +0530

    Delete employeeRecords.sql

--- employeeRecords.sql
+++
@@ -1,4 +0,0 @@
--- Customer service in each country
-
-SELECT employees.employeeNumber, employees.officeCode, employees.jobTitle, offices.country 
-FROM employees JOIN offices ON employees.officeCode= offices.officeCode;
