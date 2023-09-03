003010909fe42b67ef750c08a753429557c76ec1
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 22:57:25 +0530

    Update optimalStocks.sql

--- optimalStocks.sql
+++ optimalStocks.sql
@@ -2,12 +2,12 @@
 SELECT
     products.productName,
     products.quantityInStock,
-    (products.quantityInStock * 0.7) AS reduction_stock,
     SUM(orderdetails.quantityOrdered) AS totalQuantityOrdered,
     (products.quantityInStock - COALESCE(SUM(orderdetails.quantityOrdered), 0)) AS difference
-FROM products
-LEFT JOIN orderdetails
-ON products.productCode = orderdetails.productCode
-GROUP BY products.productName, products.quantityInStock
-HAVING (products.quantityInStock - COALESCE(SUM(orderdetails.quantityOrdered), 0)) < 3000 AND (products.quantityInStock - COALESCE(SUM(orderdetails.quantityOrdered), 0))>500
-ORDER BY totalQuantityOrdered DESC;
+    FROM products
+    LEFT JOIN orderdetails
+    ON products.productCode = orderdetails.productCode
+    GROUP BY products.productName, products.quantityInStock
+    HAVING (products.quantityInStock - COALESCE(SUM(orderdetails.quantityOrdered), 0)) < 3000 
+    AND (products.quantityInStock - COALESCE(SUM(orderdetails.quantityOrdered), 0))>500
+    ORDER BY totalQuantityOrdered DESC;
