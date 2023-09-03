02286b323d13eb867f0ab25c2eec112c8c010e36
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 18:55:43 +0530

    Delete cancelBudget.sql

--- cancelBudget.sql
+++
@@ -1,4 +0,0 @@
--- orders cancelled due to budgeting issues
-SELECT orders.status, orders.customerNumber, orderdetails.quantityOrdered, orderdetails.priceEach, orders.comments 
-FROM orders JOIN orderdetails ON orders.orderNumber= orderdetails.orderNumber 
-WHERE orders.status="Cancelled" AND orders.customerNumber=496;
