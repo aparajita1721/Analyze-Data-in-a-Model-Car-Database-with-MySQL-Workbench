963f7fe377565330b935d5fc777e92f30036ebcd
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 23:53:33 +0530

    Update urgentRestock.sql

--- urgentRestock.sql
+++ urgentRestock.sql
@@ -5,9 +5,9 @@ SELECT
     products.quantityInStock,
     SUM(orderdetails.quantityOrdered) AS totalQuantityOrdered,
     (products.quantityInStock - COALESCE(SUM(orderdetails.quantityOrdered), 0)) AS difference
-FROM products
-LEFT JOIN orderdetails
-ON products.productCode = orderdetails.productCode
-GROUP BY products.productName, products.quantityInStock
-HAVING (products.quantityInStock - COALESCE(SUM(orderdetails.quantityOrdered), 0)) < 0
-ORDER BY totalQuantityOrdered DESC;
+    FROM products
+    LEFT JOIN orderdetails
+    ON products.productCode = orderdetails.productCode
+    GROUP BY products.productName, products.quantityInStock
+    HAVING (products.quantityInStock - COALESCE(SUM(orderdetails.quantityOrdered), 0)) < 0
+    ORDER BY totalQuantityOrdered DESC;
