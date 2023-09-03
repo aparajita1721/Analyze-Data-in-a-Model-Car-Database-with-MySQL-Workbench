b620098e9920804270d5827912a1c1c5b604f22b
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 18:53:47 +0530

    Delete CancelledOrders.sql

--- CancelledOrders.sql
+++
@@ -1,7 +0,0 @@
--- customers that cancelled their order, their reasons and the quantity of products ordered
-SELECT  orders.customerNumber, orders.status, SUM(orderdetails.quantityOrdered) AS Quantity_ordered, orders.comments
-FROM orderdetails JOIN orders ON orderdetails.orderNumber=orders.orderNumber 
-WHERE orders.status="On Hold" OR orders.status="Cancelled" OR orders.status= "Disputed" OR orders.status="In Process" 
-GROUP BY orders.customerNumber ORDER BY SUM(orderdetails.quantityOrdered) DESC;
-
-
