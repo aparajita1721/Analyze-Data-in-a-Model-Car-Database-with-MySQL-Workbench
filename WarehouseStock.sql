fa2048f4cbd090de50363b7209c986cc09353c8d
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 18:55:32 +0530

    Delete WarehouseStock.sql

--- WarehouseStock.sql
+++
@@ -1,6 +0,0 @@
--- details on warehouses
-SELECT * FROM warehouses;
-
-# quantity of stock in each warehouse
-
-SELECT  warehouseCode, SUM(quantityInStock) AS stock FROM products GROUP BY warehouseCode;
