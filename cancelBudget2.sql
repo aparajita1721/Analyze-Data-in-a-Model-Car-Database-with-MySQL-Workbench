8b936d1fef8869f61e9e1e1223e53df80946a61f
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 18:55:53 +0530

    Delete cancelBudget2.sql

--- cancelBudget2.sql
+++
@@ -1,5 +0,0 @@
--- hard to please customer
-SELECT orders.status, orders.customerNumber, orderdetails.quantityOrdered, orderdetails.priceEach, orders.comments 
-FROM orders JOIN orderdetails ON orders.orderNumber= orderdetails.orderNumber 
-WHERE orders.status="Cancelled" AND orders.customerNumber=201;
-
