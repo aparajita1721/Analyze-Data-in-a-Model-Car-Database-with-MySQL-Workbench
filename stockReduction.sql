fc20f13cfdf1ad32b72b22349cd9fedd60231d55
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 23:51:52 +0530

    Update stockReduction.sql

--- stockReduction.sql
+++ stockReduction.sql
@@ -5,11 +5,11 @@ SELECT
     (products.quantityInStock * 0.7) AS reduction_stock,
     SUM(orderdetails.quantityOrdered) AS totalQuantityOrdered,
     (products.quantityInStock - COALESCE(SUM(orderdetails.quantityOrdered), 0)) AS difference
-FROM products
-LEFT JOIN orderdetails
-ON products.productCode = orderdetails.productCode
-GROUP BY products.productName, products.quantityInStock
-HAVING (products.quantityInStock - COALESCE(SUM(orderdetails.quantityOrdered), 0)) > 3000
-ORDER BY totalQuantityOrdered DESC;
+    FROM products
+    LEFT JOIN orderdetails
+    ON products.productCode = orderdetails.productCode
+    GROUP BY products.productName, products.quantityInStock
+    HAVING (products.quantityInStock - COALESCE(SUM(orderdetails.quantityOrdered), 0)) > 3000
+    ORDER BY totalQuantityOrdered DESC;
 
 
