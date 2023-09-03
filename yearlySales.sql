c6d500be2d610658fa2316165d27ee8be54963dc
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 18:58:57 +0530

    Delete yearlySales.sql

--- yearlySales.sql
+++
@@ -1,4 +0,0 @@
--- quantity of total sales each year , significant decline in the last year
-SELECT YEAR(orders.orderDate) AS Year, SUM(orderdetails.quantityOrdered) AS Total_quantity_sales
-FROM orders LEFT JOIN orderdetails ON orders.orderNumber= orderdetails.orderNumber 
-GROUP BY YEAR(orders.orderDate);
