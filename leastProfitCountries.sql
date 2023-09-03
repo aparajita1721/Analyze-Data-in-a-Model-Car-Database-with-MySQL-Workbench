6e1f3783a68e1ba2c089fedde5f09f3e24d5e17e
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 22:42:30 +0530

    Update leastProfitCountries.sql

--- leastProfitCountries.sql
+++ leastProfitCountries.sql
@@ -1,4 +1,4 @@
 -- Least profits earned in which countries
 SELECT  customers.country, SUM(payments.amount) AS Amount FROM customers 
 LEFT JOIN payments ON customers.customerNumber= payments.customerNumber 
-GROUP BY customers.country ORDER BY SUM(payments.amount) ASC;
+GROUP BY customers.country ORDER BY SUM(payments.amount) ASC LIMIT 10;
