0cf50880b3347ae4059bb7f8fae4a5e93a012daa
Aparajita Saha <143907878+aparajita1721@users.noreply.github.com>
2023-09-03 18:57:26 +0530

    Delete mostPopularProductsStock.sql

--- mostPopularProductsStock.sql
+++
@@ -1,5 +0,0 @@
--- displaying top 10 Product names and product lines that were ordered the most and their stock availability
-SELECT products.productName, products.productLine, 
-products.quantityInStock, SUM(orderdetails.quantityOrdered) AS quantity_ordered 
-FROM products LEFT JOIN orderdetails ON products.productCode= orderdetails.productCode 
-GROUP BY products.productName ORDER BY SUM(orderdetails.quantityOrdered) DESC LIMIT 10;
